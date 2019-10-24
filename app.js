2be6131d013348432fe503dc709926e7c5ef4758
springunicorn <email>
2019-10-24 23:44:43 +0000

    Added ejs starter files

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 var express = require("express");
 var app = express();
 
+// Express will serve the files in the "public" directory
+// Put css files there
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/bye", function(req, res){
