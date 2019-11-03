4a840710c4e0994829a923670dd0f3afd4b0b59a
springunicorn <email>
2019-11-03 06:54:34 +0000

    Added mongoose start up code

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 var express = require("express");
 var app = express();
 
+var mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect('mongodb://127.0.0.1/test');
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(){
+   console.log("Mongoose connected.") 
+});
+
 // Express will serve the files in the "public" directory
 // Put css files there
 app.use(express.static("public"));
@@ -13,6 +26,10 @@ app.get("/", function(req, res){
 
 app.get("/bye", function(req, res){
     res.send("Bye bye");
+})
+
+app.post("/signup",  function(req, res){
+    
 })
 
 app.listen(8080, function() {
